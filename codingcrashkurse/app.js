22e678a20357152cbcd854b9c116faf7b74dcd46
chefderif <abdellatif.laaroussi@gmx.de>
2023-03-15 20:42:09 +0100

    index computed and two way data binding

--- codingcrashkurse/app.js
+++ codingcrashkurse/app.js
@@ -1,6 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'James',
+        name: 'James',       
+        lastName: "",
+        firstName: "",
+        fullName: this.lastName + " " + this.firstName,
+        website: 'http://www.google.de'
     },
+    methods: {
+        sayHi: function(){
+            return 'TEST  TEST';
+        }
+    }
 });
